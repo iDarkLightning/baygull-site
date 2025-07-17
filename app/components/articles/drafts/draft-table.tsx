3a027eedb9467e6e159444a30fa4037417bf64c5
Nirjhor Nath <nirjhor.nath@gmail.com>
2025-07-16 23:45:13 -0400

    drafts empty state

--- app/components/articles/drafts/draft-table.tsx
+++ app/components/articles/drafts/draft-table.tsx
@@ -229,38 +229,73 @@ export const DraftTable = () => {
     ]);
   }, [state]);
 
+  const isFilterActive =
+    state.titleDesc !== "" ||
+    state.authors.size > 0 ||
+    state.statuses.size > 0 ||
+    state.submissionTime !== null;
+
   return (
     <div className="px-1 md:px-6 overflow-auto">
-      <table className="w-full">
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id} className="text-left">
-              {headerGroup.headers.map((header) => (
-                <th
-                  key={header.id}
-                  className="font-semibold text-sm text-neutral-700 py-2"
-                >
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext()
-                  )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {table.getRowModel().rows.length === 0 && (
+        <div className="flex flex-col mx-auto justify-center my-32 w-fit max-w-sm gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-12 text-neutral-400"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m7.875 14.25 1.214 1.942a2.25 2.25 0 0 0 1.908 1.058h2.006c.776 0 1.497-.4 1.908-1.058l1.214-1.942M2.41 9h4.636a2.25 2.25 0 0 1 1.872 1.002l.164.246a2.25 2.25 0 0 0 1.872 1.002h2.092a2.25 2.25 0 0 0 1.872-1.002l.164-.246A2.25 2.25 0 0 1 16.954 9h4.636M2.41 9a2.25 2.25 0 0 0-.16.832V12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 12V9.832c0-.287-.055-.57-.16-.832M2.41 9a2.25 2.25 0 0 1 .382-.632l3.285-3.832a2.25 2.25 0 0 1 1.708-.786h8.43c.657 0 1.281.287 1.709.786l3.284 3.832c.163.19.291.404.382.632M4.5 20.25h15A2.25 2.25 0 0 0 21.75 18v-2.625c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125V18a2.25 2.25 0 0 0 2.25 2.25Z"
+            />
+          </svg>
+
+          <p className="text- font-semibold text-neutral-700">
+            Submitted Drafts
+          </p>
+          <p className="text-sm text-neutral-500">
+            {isFilterActive
+              ? "There are no drafts that match the selected filters"
+              : "Article drafts are all submissions made by authors for potential review. Currently there are no article drafts."}
+          </p>
+        </div>
+      )}
+      {table.getRowModel().rows.length > 0 && (
+        <table className="w-full">
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id} className="text-left">
+                {headerGroup.headers.map((header) => (
+                  <th
+                    key={header.id}
+                    className="font-semibold text-sm text-neutral-700 py-2"
+                  >
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
