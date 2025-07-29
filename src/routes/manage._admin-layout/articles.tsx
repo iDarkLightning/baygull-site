cd5a3759180b4e27a0a6426efff9e98d5c2b7da0
Nirjhor Nath <nirjhor.nath@gmail.com>
2025-07-29 01:38:02 -0400

    delete old articles manage file

--- src/routes/manage._admin-layout/articles.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/manage/_admin-layout/articles")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello "/manage/_admin-layout/articles"!</div>;
-}
