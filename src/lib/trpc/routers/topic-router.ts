e9d2679c507666671360a119b691270bafc02f61
Nirjhor Nath <nirjhor.nath@gmail.com>
2025-08-03 18:46:32 -0400

    create topic procedure

--- src/lib/trpc/routers/topic-router.ts
+++ src/lib/trpc/routers/topic-router.ts
@@ -1,7 +1,29 @@
-import { authedProcedure } from "../middleware/auth-middleware";
+import { isCuid } from "@paralleldrive/cuid2";
+import { z } from "zod";
+import { topic } from "~/lib/db/schema";
+import { publicProcedure } from "../init";
+import { adminProcedure } from "../middleware/auth-middleware";
 
 export const topicRouter = {
-  getAll: authedProcedure.query(async ({ ctx }) => {
-    return await ctx.db.query.topic.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    return ctx.db.query.topic.findMany();
   }),
+
+  create: adminProcedure
+    .input(
+      z.object({
+        id: z.string().refine((id) => isCuid(id)),
+        name: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const [result] = await ctx.db
+        .insert(topic)
+        .values({
+          name: input.name,
+        })
+        .returning();
+
+      return result;
+    }),
 };
