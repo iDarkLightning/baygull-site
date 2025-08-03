44d4cefcdf45a92a7517bc985ced50c0c02d3e45
Nirjhor Nath <nirjhor.nath@gmail.com>
2025-08-03 18:46:16 -0400

    add option to include authed user for getUsers

--- src/lib/trpc/routers/user-router.ts
+++ src/lib/trpc/routers/user-router.ts
@@ -2,22 +2,29 @@ import { eq, not } from "drizzle-orm";
 import { publicProcedure } from "../init";
 import { authedProcedure, extractAuth } from "../middleware/auth-middleware";
 import { user } from "~/lib/db/schema";
+import { z } from "zod";
 
 export const userRouter = {
   me: publicProcedure
     .use(extractAuth)
     .query(async ({ ctx }) => ctx.user ?? null),
 
-  getUsers: authedProcedure.query(async ({ ctx }) => {
-    const users = await ctx.db.query.user.findMany({
-      where: not(eq(user.id, ctx.user.id)),
-      columns: {
-        name: true,
-        id: true,
-        image: true,
-      },
-    });
+  getUsers: authedProcedure
+    .input(
+      z.object({
+        includeMe: z.boolean(),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const users = await ctx.db.query.user.findMany({
+        where: input.includeMe ? undefined : not(eq(user.id, ctx.user.id)),
+        columns: {
+          name: true,
+          id: true,
+          image: true,
+        },
+      });
 
-    return users;
-  }),
+      return users;
+    }),
 };
