0b6e0eebe38b7a689273add07dcde53eede857f7
Nirjhor Nath <nirjhor.nath@gmail.com>
2025-08-05 20:33:02 -0400

    match outline button to input

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -22,7 +22,7 @@ export const button = cva(
         primary:
           "bg-neutral-800 hover:bg-neutral-800/85 text-white disabled:hover:bg-neutral-300",
         outline:
-          "border-[0.0125rem] border-zinc-300/70 hover:bg-neutral-100 dark:hover:bg-neutral-700 disabled:hover:bg-transparent",
+          "border-[0.0125rem] border-zinc-300/70 hover:border-zinc-400/80 disabled:hover:bg-transparent shadow-xs",
         secondary:
           "bg-sky-100 text-sky-900 border-[0.0125rem] border-sky-700/60 hover:bg-sky-200/70",
         ghost:
