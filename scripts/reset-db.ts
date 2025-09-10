9d736ca4320d3390e09af286191a65781685dcf3
Nirjhor Nath <nirjhor.nath@gmail.com>
2025-09-10 15:33:02 -0400

    create reset db script

---
+++ scripts/reset-db.ts
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+import { sql } from "drizzle-orm";
+
+dotenv.config({
+  path: "../.env",
+});
+
+const resetDB = async () => {
+  const { db } = await import("@baygull/db");
+
+  console.log("\n---FETCHING TABLES TO DELETE---");
+  const { rows: tables } = await db.run(
+    sql`SELECT name FROM sqlite_master WHERE type='table';`
+  );
+  console.log("---DELETING ALL VALUES FROM THE FOLLOWING TABLES---\n");
+
+  tables.forEach((table) => console.log(table.name));
+
+  await db.run(sql`PRAGMA foreign_keys = OFF;`);
+
+  await Promise.all(
+    tables.map((table) => db.run(`DELETE FROM ${table.name};`))
+  );
+
+  console.log("\n\n---DATABASE RESET---");
+
+  await db.run(sql`PRAGMA foreign_keys = ON;`);
+};
+
+resetDB().catch(console.error);
